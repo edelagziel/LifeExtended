9bcb1292d7efd8f094da91fa54768009f25a5471
Eden Lagziel <edenlagziel63@gmail.com>
2025-12-07 12:15:22 +0200

    base Homepage

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+import Home from "./components/Home";
+
+function App()
+{
+  return(
+    <>
+        <h1>lifeextended</h1>
+        <Home />
+    </>
+  );
+}
+
+export default App;
